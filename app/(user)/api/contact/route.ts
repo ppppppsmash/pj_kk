4ddbf7f4015d3e5c45a87daa7d3371ca6a6189ea
ppppppsmash <pppppp.work@gmail.com>
2024-10-07 19:31:42 +0900

    fix: Fix auto-reply for contact form submissions

--- app/(user)/api/contact/route.ts
+++ app/(user)/api/contact/route.ts
@@ -1,6 +1,15 @@
 //import { contactFormSchema } from '@/data/schemas/contact-form-schema'
 import nodemailer, { SendMailOptions } from 'nodemailer'
 
+const keyTransform: { [key: string]: string} = {
+  familyname: '姓',
+  lastname: '名',
+  email: 'メール',
+  tel: '電話番号',
+  companyname: '会社名',
+  mainmessage: 'お問い合わせ内容',
+}
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   port: 465,
@@ -20,37 +29,29 @@ const sendMail = (mailOptions: SendMailOptions) =>
 export async function POST(request: Request) {
   const formData = await request.json()
 
-  console.log(formData)
-
-  //const validatonResult = contactFormSchema.safeParse(formData);
-
-  // if (!formData.success) {
-  //   const error = formData.error.format()
-
-  //   return Response.json(
-  //     {
-  //       success: false,
-  //       reason: error,
-  //     },
-  //     {
-  //       status: 422,
-  //     },
-  //   );
-  // }
-
-  const formattedText = Object.entries(formData)
-    .map(([key, value]) => `${key}: ${value}`)
+  const formattedTextForContact = Object.entries(formData)
+    .map(([key, value]) => `${keyTransform[key] || key}: ${value}`)
     .join('\n');
+
+  const formattedTextForUser = 'お問い合わせありがとうございます。以下の内容で受け付けました。\n' + formattedTextForContact + '\n\n恒佳よりご連絡させていただきますので、しばらくお待ち下さい。\nよろしくお願いいたします。';
 
   const mailOptions = {
     from: process.env.EMAIL_SENDER,
     to: process.env.EMAIL_DESTINATION,
     subject: 'お問い合わせの件',
-    text: formattedText,
+    text: formattedTextForContact,
+  }
+
+  const mailOptionsForUser = {
+    from: process.env.EMAIL_SENDER,
+    to: formData.email, // ユーザーのメールアドレス
+    subject: 'お問い合わせありがとうございます',
+    text: formattedTextForUser,
   }
 
   try {
     await sendMail(mailOptions);
+    await sendMail(mailOptionsForUser);
 
     return Response.json({ success: true })
   } catch (error) {
