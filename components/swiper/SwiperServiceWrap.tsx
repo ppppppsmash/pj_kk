b4e752db51dab1f53fe258f00d24ed6168d59c48
ppppppsmash <pppppp.work@gmail.com>
2024-12-12 23:00:06 +0900

    fix: product area

--- components/swiper/SwiperServiceWrap.tsx
+++ components/swiper/SwiperServiceWrap.tsx
@@ -11,6 +11,7 @@ interface Slide {
   link: string
   title: string
   date: string
+  description: string
 }
 
 interface Slides {
@@ -31,19 +32,20 @@ export const SwiperServiceWrap = ({ slides }: Slides) => {
       onSwiper={(swiper) => console.log(swiper)}
     >
       {slides.map((slide, index) => (
-        <SwiperSlide className="!w-[300px] !h-[360px]" key={index}>
+        <SwiperSlide className="!w-[300px] !h-[200px]" key={index}>
           <div className="relative h-full rounded-[30px] overflow-hidden shadow-[6px_4px_15px_0_rgba(51,51,51,.19)]">
             <Link href={slide.link} className="block h-full pb-6 bg-white overflow-hidden relative z-10">
-              <div className="h-full sm:mb-4 overflow-hidden rounded-[50px]">
+              <div className="w-full max-w-full h-[120px] sm:h-[160px] rounded-[50px]">
                 <img
                   src={slide.imgThumb}
-                  className="w-full h-full object-cover duration-300 hover:scale-[1.2]"
+                  width="300"
+                  className="duration-300 hover:scale-[1.2]"
                 />
               </div>
 
-              <h3 className="text-white absolute right-0 bottom-0 w-[calc(100%_-30px)] bg-[rgba(32,24,1,.5)] rounded-tl-[20px] p-2">
+              <h3 className="text-white absolute right-0 bottom-0 bg-[rgba(32,24,1,.5)] pt-2 pl-4 pb-2 w-full">
                 <span className="text-[18px] mb-2 block leading-[1.5] font-semibold">{slide.title}</span>
-                <span className="text-[12px] block leading-[1]">{slide.title}</span>
+                <span className="text-[12px] block leading-[1]">{slide.description}</span>
               </h3>
             </Link>
           </div>
