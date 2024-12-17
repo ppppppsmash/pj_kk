47c6be59bb5eb2dcf70adf49a20afc0f6470f1c6
ppppppsmash <pppppp.work@gmail.com>
2024-12-17 21:04:33 +0900

    fix: fix type

--- app/(user)/product/[slug]/page.tsx
+++ app/(user)/product/[slug]/page.tsx
@@ -6,7 +6,6 @@ interface Product {
   id: number;
   imgThumb: string;
   product_detail?: ProductDetail[];
-  productImg: string[];
   link: string;
   title: string;
   description: string;
@@ -36,26 +35,23 @@ export default function ProductPage({ params } : { params: { slug: string }}) {
     <div>
       <section className="pt-[50px] sm:pt-[100px] bg-gray w-[100vw] overflow-hidden pb-10">
         <div className="pt-[60px] px-[10%] max-w-[1600px] mx-auto">
-          <h1
-            className="font-bold text-[30px] sm:text-[50px] leading-[50px] sm:leading-[70px] tracking-[.01em] duration-100 delay-100"
-          >
-            {product.title}
-          </h1>
-
-          <div className="mt-[30px] mx-auto tracking-wide text-[#545454]">
-            <h3 className="font-semibold leading-[27px] text-[30px]">
-              ブランド名： {product.brand}
-            </h3>
-
-            <div className="flex justify-between items-center mt-6">
-              <p>カテゴリー： {product.category}</p>
-              <p>2024.03.27</p>
-            </div>
-          </div>
         </div>
       </section>
 
       <section className="px-4 py-10 sm:px-[100px]">
+        <h1
+          className="font-bold text-[30px] sm:text-[50px] leading-[50px] sm:leading-[70px] tracking-[.01em] duration-100 delay-100"
+        >
+          {product.title}
+        </h1>
+
+        <div className="mt-[30px] mx-auto tracking-wide text-[#545454]">
+          <div className="flex justify-between items-center mt-6">
+            <p>カテゴリー： {product.category}</p>
+            <p>2024.03.27</p>
+          </div>
+        </div>
+
         {product.product_detail && <AnimatedTestimonials testimonials={product.product_detail} />}
 
         <div className="max-w-[800px] w-full mx-auto mt-5 mb-16">
